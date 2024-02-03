d60051110bb27ad3c27ab86f848a4555462570cc
Shaun Lalani <lalani.shaun@gmail.com>
2024-02-03 17:41:55 -0500

    random

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![allow(unused)]
+
+use std::io; 
+use rand::Rng; 
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File; 
+use std::cmp::Ordering; 
+
+// Random
 fn main() {
-    println!("Hello, world!");
+    // cal the random thread to generate a random number from a range. 
+    let rand_num = rand::thread_rng().gen_range(1..101); 
+    println!("Random fucking number: {}", rand_num); 
+
 }
+
